677646499c7736e83f06e5ac61ae59770a8d84a9
Brayan Llanos <brayan0428@gmail.com>
2020-02-07 15:36:31 -0500

    Firts steps add

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,4 @@
+module.exports = (err,req,res,next) => {
+    const httpStatus = err.status || 500
+    res.status(httpStatus).send({status:httpStatus, message: err.message || "Internal server error"})
+}
---
+++ src/services/home.service.js
@@ -0,0 +1,9 @@
+class HomeService{
+    index(){
+        return{
+            message: "Hellor World"
+        }
+    }
+}
+
+module.exports = HomeService
